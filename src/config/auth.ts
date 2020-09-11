f58ecc0a09455aa2d50c2270a642ca709cdc98f6
Eder Chiquito <eder.almeida40@gmail.com>
2020-09-10 22:29:41 -0300

    created middleware to check if user is authenticated, saving it's id in the  request type

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'b09047b7872053dc2b06e78ab3de3ea9',
+        expiresIn: '1d',
+    },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -25,9 +26,12 @@ class AuthenticateUserService {
         if (!passwordMatched) {
             throw new Error('Incorrect email/password combination');
         }
-        const token = sign({}, 'b09047b7872053dc2b06e78ab3de3ea9', {
+
+        const { expiresIn, secret } = authConfig.jwt;
+
+        const token = sign({}, secret, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn,
         });
 
         return {
